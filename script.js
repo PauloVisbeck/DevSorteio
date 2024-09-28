22e29892e9dccf20a652512295647da2b4319934
PauloVisbeck <pls_ray@yahoo.com.br>
2024-09-27 21:10:17 -0300

    Quarta versao. Alteracao no arquivo JS. Inclusao de verificacao se o segundo numero eh maior do que o primeiro.

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 let numMin = document.getElementById("numero-minimo")
+numMin.addEventListener("change", function () {
+   console.log(numMin.value)
+})
+
 
 let numMax = document.getElementById("numero-maximo")
+numMax.addEventListener("change", function () {
+   console.log(numMax.value)
+
+   // Verifica se o segundo número é maior
+   if (Number(numMax.value) <= Number(numMin.value)) {
+      alert("O segundo valor precisa ser maior do que o primeiro!")
+      numMax.value = ""
+      numMax.focus()
+   }
+})
 
 
 const botao = document.getElementById("sortear")
-botao.addEventListener("click", function () {    
+botao.addEventListener("click", function () {
+   // Testo novamente para garantir se o usuário não insistiu em um valor menor
+   if (Number(numMax.value) <= Number(numMin.value)) {
+      alert("O segundo valor precisa ser maior do que o primeiro!")
+      numMax.value = ""
+      numMax.focus()
+      return
+   }
 
-    let valorRandom = Math.random()
+   let valorRandom = Math.random()
+   
+   let resultado = Math.floor(valorRandom * (Number(numMax.value) - Number(numMin.value) + 1)) + Number(numMin.value)
 
+   console.log("Resultado Final: ", resultado)
 
-    let resultado = Math.floor(valorRandom * (Number(numMax.value) - Number(numMin.value) + 1)) + Number(numMin.value)
-
-    console.log("Resultado Final: ", resultado)
-
-    const span = document.getElementById("resultado")
-    span.textContent = resultado
-}) 
+   const span = document.getElementById("resultado")
+   span.textContent = resultado
+})
